509fcd38f4fc6150db74bbb3edc2fb40310b2b4b
SAIKIRAN REDDY GANGIDI <s541902@nwmissouri.edu>
2021-03-31 19:01:32 -0500

    final commit

--- routes/earphones.js
+++ routes/earphones.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('earphones', { title: 'Search Results electricity' });
+  res.render('earphones', { title: 'Search Results earphone' });
 });
 
 module.exports = router;
